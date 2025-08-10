94b435f7ba8621ef6d83abec290cfc00274ca78e
alwinjose682 <alwinjoseth@gmail.com>
2025-08-10 17:25:18 +0200

    Modified the default initial TradeId and FoCashflowId

--- fo-simulator/src/main/java/io/alw/css/fosimulator/definition/IdProvider.java
+++ fo-simulator/src/main/java/io/alw/css/fosimulator/definition/IdProvider.java
@@ -4,13 +4,15 @@ import java.util.concurrent.atomic.AtomicLong;
 
 final class IdProvider {
     private static IdProvider instance;
+    private static final long defaultInitialTradeId = 1000000L;
+    private static final long defaultInitialFoCfId = 10000L;
 
     private final AtomicLong tradeIdGenerator;
     private final AtomicLong foCfIdGenerator;
 
     private IdProvider() {
-        this.tradeIdGenerator = new AtomicLong(100L);
-        this.foCfIdGenerator = new AtomicLong(10L);
+        this.tradeIdGenerator = new AtomicLong(defaultInitialTradeId);
+        this.foCfIdGenerator = new AtomicLong(defaultInitialFoCfId);
     }
 
     static IdProvider singleton() {
