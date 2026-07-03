6b075c8e8c9107aa4ac4e1fc9e14649b0ec17e7f
alwin <alwinjoseth@gmail.com>
2026-07-03 15:06:56 +0200

    minor changes renamed the config directories: fo-simulator to trade-publisher cashflow-consumer to trade-consumer

--- css-lib/css-scripts/build/buildAll.sh
+++ css-lib/css-scripts/build/buildAll.sh
@@ -6,8 +6,8 @@
 ./install.sh refdata-generator
 ./install.sh css-infra/ignite-cache/
 ./install.sh db-cache-data-loader
-./install.sh fo-simulator
-./install.sh cashflow-consumer
+./install.sh trade-publisher
+./install.sh trade-consumer
 
 # Build H2 DB to be run in server mode. OracleDB can be used instead of H2 when needed with config changes in relevant places like pom etc
 ./install.sh css-infra/h2-server/
