b9086d2c9cf78d7c935b705eed567a5e39f14bcf
alwin <alwinjoseth@gmail.com>
2026-07-11 22:50:12 +0200

    database: grants for confirmation_match_status

--- css-infra/oracle-db/install__1_0/scripts/grants/grants_css.sql
+++ css-infra/oracle-db/install__1_0/scripts/grants/grants_css.sql
@@ -1,7 +1,9 @@
 grant select on cashflow to css;
 grant select on cashflow_rejection to css;
 grant select on trade_link to css;
+grant select on confirmation_match_status to css;
 
 grant update, delete, insert on cashflow to css;
 grant update, delete, insert on cashflow_rejection to css;
 grant update, delete, insert on trade_link to css;
+grant update, delete, insert on confirmation_match_status to css;
