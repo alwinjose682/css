6e7b8264c208d4c4334990be6cf1266b548696bb
alwin <alwinjoseth@gmail.com>
2026-07-12 00:26:31 +0200

    logging improvement

--- trade-consumer/src/main/java/io/alw/css/tradeconsumer/cashflow/KafkaTradeConsumer.java
+++ trade-consumer/src/main/java/io/alw/css/tradeconsumer/cashflow/KafkaTradeConsumer.java
@@ -3,12 +3,15 @@ package io.alw.css.tradeconsumer.cashflow;
 import io.alw.css.domain.common.InputBy;
 import io.alw.css.serialization.trade.TradeAvro;
 import io.alw.css.tradeconsumer.cashflow.service.TradeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaTradeConsumer {
+    private final static Logger log = LoggerFactory.getLogger(KafkaTradeConsumer.class);
     private final TradeService tradeService;
 
     public KafkaTradeConsumer(TradeService tradeService) {
@@ -17,6 +20,13 @@ public class KafkaTradeConsumer {
 
     @KafkaListener(topics = "${app.kafka.topic.trade-cash-generation-event}", groupId = "${spring.kafka.consumer.group-id}", containerFactory = "tradeCashGenerationListenerContainerFactory")
     public void accept(Message<TradeAvro> message) {
-        tradeService.process(message.getPayload(), InputBy.CSS_SYS);
+        var tradeAvro = message.getPayload();
+        final long tradeId = tradeAvro.getTradeID();
+        final int tradeVersion = tradeAvro.getTradeVersion();
+        final String tradeType = tradeAvro.getTradeType();
+        final int numOfTradeLegs = tradeAvro.getTradeLegs().size();
+        log.info("Received TradeAvroMessage[tradeId: {}, tradeVersion: {}, tradeType: {}] with {} trade legs", tradeId, tradeVersion, tradeType, numOfTradeLegs);
+
+        tradeService.process(tradeAvro, InputBy.CSS_SYS);
     }
 }
--- trade-consumer/src/main/java/io/alw/css/tradeconsumer/confirmation/KafkaConsumer.java
+++ trade-consumer/src/main/java/io/alw/css/tradeconsumer/confirmation/KafkaConsumer.java
@@ -1,14 +1,16 @@
 package io.alw.css.tradeconsumer.confirmation;
 
-import io.alw.css.domain.common.InputBy;
 import io.alw.css.serialization.confirmation.ConfirmationMatchStatusAvro;
 import io.alw.css.tradeconsumer.confirmation.service.TradeConfirmationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
+    private final static Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
     private final TradeConfirmationService tradeConfirmationService;
 
     public KafkaConsumer(TradeConfirmationService tradeConfirmationService) {
@@ -17,6 +19,13 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "${app.kafka.topic.confirmation-match-status}", groupId = "${spring.kafka.consumer.group-id}", containerFactory = "confMatchStatusListenerContainerFactory")
     public void accept(Message<ConfirmationMatchStatusAvro> message) {
-        tradeConfirmationService.process(message.getPayload(), InputBy.CSS_SYS);
+        var avro = message.getPayload();
+        long tradeId = avro.getTradeId();
+        int tradeVersion = avro.getTradeVersion();
+        String tradeType = avro.getTradeType();
+        int numOfTradeLegs = avro.getTradeLegMatchAttributes().size();
+        log.info("Received TradeAvroMessage[tradeId: {}, tradeVersion: {}, tradeType: {}] with {} trade legs", tradeId, tradeVersion, tradeType, numOfTradeLegs);
+
+        tradeConfirmationService.process(avro);
     }
 }
