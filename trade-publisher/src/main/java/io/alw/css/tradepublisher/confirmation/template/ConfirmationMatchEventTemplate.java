02ec53a2a563560bc0667905c504f7deb3e55525
alwin <alwinjoseth@gmail.com>
2026-08-02 07:43:13 +0200

    Added logging for ConfirmationMatchEventTemplate

--- trade-publisher/src/main/java/io/alw/css/tradepublisher/confirmation/template/ConfirmationMatchEventTemplate.java
+++ trade-publisher/src/main/java/io/alw/css/tradepublisher/confirmation/template/ConfirmationMatchEventTemplate.java
@@ -4,6 +4,7 @@ import io.alw.css.confirmation.ConfirmationMatchEvent;
 import io.alw.css.confirmation.ConfirmationMatchRequest;
 import io.alw.css.confirmation.MatchStatus;
 import io.alw.css.confirmation.TradeLegMatchAttribute;
+import io.alw.css.domain.common.TradeType;
 import io.alw.css.tradepublisher.IdProvider;
 import io.alw.css.tradepublisher.confirmation.ConfirmationMatchEventPublisher;
 import io.alw.css.tradepublisher.generator.DayTicker;
@@ -11,6 +12,8 @@ import io.alw.css.tradepublisher.store.ExtendedInMemoryStore;
 import io.alw.css.tradepublisher.store.ExtendedStoreHelper;
 import io.alw.css.tradepublisher.store.StoreHelper;
 import io.alw.css.tradepublisher.trade.service.RefDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,6 +25,7 @@ import java.util.random.RandomGenerator;
 
 public final class ConfirmationMatchEventTemplate implements Supplier<List<ConfirmationMatchEvent>> {
     private static final int VERSION_ONE = 1;
+    private static final Logger log = LoggerFactory.getLogger(ConfirmationMatchEventTemplate.class);
 
     private final DayTicker dayTicker;
     private final RefDataService refDataService;
@@ -57,22 +61,23 @@ public final class ConfirmationMatchEventTemplate implements Supplier<List<Confi
             // Note: The previous matchRequest may be present in only one of the store or it may not be present even in both the stores.
             // It is safe to remove without checking if it exists or not.
             // The same applies to matchEvent
-            matchRequestStoreHelper.removeById(contraPairId, StoreHelper.Purpose.ITEM_SPECIFIC_EVENT);
-            matchEventStoreHelper.removeById(contraPairId, StoreHelper.Purpose.AMEND);
+            boolean remResult1 = matchRequestStoreHelper.removeById(contraPairId, StoreHelper.Purpose.ITEM_SPECIFIC_EVENT);
+            boolean remResult2 = matchEventStoreHelper.removeById(contraPairId, StoreHelper.Purpose.AMEND);
+            if (remResult1 || remResult2) {
+                log.debug("Received amended ConfirmationMatchRequest. Removed previous MatchRequest and/or MatchEvent as candidates for matching. ConfRequestId: {}, ContraPairId: {}", matchRequest.requestId(), matchRequest.contraPairId());
+            }
         }
 
         if (isMatchEventGeneratableForFutureDate(matchRequest)) {
-            matchRequestStoreHelper.storeForFutureRndmRetrievalDay(matchRequest, StoreHelper.Purpose.ITEM_SPECIFIC_EVENT);
+            saveMatchRequest(matchRequest, StoreHelper.Purpose.ITEM_SPECIFIC_EVENT);
+            log.info("Saved ConfirmationMatchRequest for future match event generation. ConfMatchRequest-Id: {}, TradeType: {}, TradeId-Ver: {}-{}", matchRequest.requestId(), matchRequest.tradeType(), matchRequest.tradeId(), matchRequest.tradeVersion());
         } else {
-            updateTemplateDay();
+            newBuildCycle();
             ConfirmationMatchEvent matchEvent = generateMatchEvent(matchRequest);
-            saveForFutureAmendment(matchEvent);
+            saveMatchEventForFutureAmendment(matchEvent);
+            log.info("Generated 1 ConfirmationMatchEvent for publishing to CSS");
             eventPublisher.accept(List.of(matchEvent));
         }
-    }
-
-    private boolean isMatchRequestAnAmendment(ConfirmationMatchRequest matchRequest) {
-        return matchRequest.contraPairId() != null;
     }
 
     @Override
@@ -80,15 +85,16 @@ public final class ConfirmationMatchEventTemplate implements Supplier<List<Confi
         List<ConfirmationMatchEvent> matchEvents = new ArrayList<>();
 
         // Update template day
-        updateTemplateDay();
+        newBuildCycle();
         // Get saved match requests for current day
         Collection<ConfirmationMatchRequest> matchRequests = matchRequestStoreHelper.remove(StoreHelper.Purpose.ITEM_SPECIFIC_EVENT, currentDayForTemplate());
         matchRequests.forEach(req -> matchEvents.add(generateMatchEvent(req)));
         // Get saved match events for amendment for current day
         Collection<ConfirmationMatchEvent> eventsForAmendment = matchEventStoreHelper.remove(StoreHelper.Purpose.AMEND, currentDayForTemplate());
         eventsForAmendment.stream().map(evt -> matchEvents.add(generateAmendedMatchEvent(evt)));
+        log.info("Generated {} ConfirmationMatchEvents for publishing to CSS", matchEvents.size());
         // Store the newly generated events for future amendment
-        matchEvents.forEach(this::saveForFutureAmendment);
+        matchEvents.forEach(this::saveMatchEventForFutureAmendment);
         // Return all the generated events
         return matchEvents;
     }
@@ -97,15 +103,26 @@ public final class ConfirmationMatchEventTemplate implements Supplier<List<Confi
         MatchStatus matchStatus = getRndmMatchStatusForAmendMatchRequest();
         Set<TradeLegMatchAttribute> tradeLegMatchAttributes = getTradeLegMatchAttributes(evt.tradeLegMatchAttributes(), matchStatus);
 
+        long eventId = evt.eventId();
+        int eventVersion = evt.eventVersion() + 1;
+        long tradeId = evt.tradeId();
+        int tradeVersion = evt.tradeVersion();
+        long requestId = evt.matchRequestId();
+        Long contraPairReqId = evt.contraPairReqId();
+        TradeType tradeType = evt.tradeType();
+
+        log.debug("Generated ConfirmationMatchEvent[{}-{}] with MatchStatus: {} for TradeType: {}, TradeId-Ver: {}-{}, ConfRequestId: {}, ContraPairId: {}",
+                eventId, eventVersion, matchStatus, tradeType, tradeId, tradeVersion, requestId, contraPairReqId);
+
         return new ConfirmationMatchEvent(
-                evt.eventId(),
-                evt.eventVersion() + 1,
-                evt.tradeId(),
-                evt.tradeVersion(),
-                evt.matchRequestId(),
-                evt.contraPairReqId(),
+                eventId,
+                eventVersion,
+                tradeId,
+                tradeVersion,
+                requestId,
+                contraPairReqId,
                 tradeLegMatchAttributes,
-                evt.tradeType(),
+                tradeType,
                 matchStatus,
                 currentDateForTemplate()
         );
@@ -116,15 +133,25 @@ public final class ConfirmationMatchEventTemplate implements Supplier<List<Confi
         MatchStatus matchStatus = getRndmMatchStatusForInitialMatchRequest();
         Set<TradeLegMatchAttribute> tradeLegMatchAttributes = getTradeLegMatchAttributes(matchRequest.tradeLegMatchAttributes(), matchStatus);
 
+        long tradeId = matchRequest.tradeId();
+        int tradeVersion = matchRequest.tradeVersion();
+        long requestId = matchRequest.requestId();
+        Long contraPairId = matchRequest.contraPairId();
+        TradeType tradeType = matchRequest.tradeType();
+        int versionOne = VERSION_ONE;
+
+        log.debug("Generated ConfirmationMatchEvent[{}-{}] with MatchStatus: {} for TradeType: {}, TradeId-Ver: {}-{}, ConfRequestId: {}, ContraPairId: {}",
+                eventId, versionOne, matchStatus, tradeType, tradeId, tradeVersion, requestId, contraPairId);
+
         return new ConfirmationMatchEvent(
                 eventId,
-                VERSION_ONE,
-                matchRequest.tradeId(),
-                matchRequest.tradeVersion(),
-                matchRequest.requestId(),
-                matchRequest.contraPairId(),
+                versionOne,
+                tradeId,
+                tradeVersion,
+                requestId,
+                contraPairId,
                 tradeLegMatchAttributes,
-                matchRequest.tradeType(),
+                tradeType,
                 matchStatus,
                 currentDateForTemplate()
         );
@@ -180,23 +207,32 @@ public final class ConfirmationMatchEventTemplate implements Supplier<List<Confi
                         .isBefore(getFutureDateForTemplateRelativeToCurrentDate(10)));
     }
 
+    private boolean isMatchRequestAnAmendment(ConfirmationMatchRequest matchRequest) {
+        return matchRequest.contraPairId() != null;
+    }
+
     /// This method is the starting point to start a new build cycle
     /// This method ensures that the same [DayTicker#day()] is used at all points of building multiple trades in current cycle
-    private void updateTemplateDay() {
+    private void newBuildCycle() {
         if (dayTicker.day() != currentDayForTemplate()) {
             setDayForTrdTemplate(dayTicker.day());
         }
+        log.info("Started new ConfirmationMatchEvent build cycle for day: {}", currentDayForTemplate());
     }
 
     /// Checks and save for future amendment if feasible. There is a randomness here as well
-    private void saveForFutureAmendment(ConfirmationMatchEvent matchEvent) {
+    private void saveMatchEventForFutureAmendment(ConfirmationMatchEvent matchEvent) {
         boolean canSave = rndm.nextInt(1, 50) > 30
                 && matchEvent.tradeLegMatchAttributes().stream().anyMatch(attr -> attr.valueDate().isAfter(currentDateForTemplate()));
         if (canSave) {
             matchEventStoreHelper.storeForFutureRndmRetrievalDay(matchEvent, StoreHelper.Purpose.AMEND);
+            log.debug("Saved ConfirmationMatchEvent for future amendment. ConfMatchEvent-Id: {}-{}, TradeType: {}, TradeId-Ver: {}-{}", matchEvent.eventId(), matchEvent.eventVersion(), matchEvent.tradeType(), matchEvent.tradeId(), matchEvent.tradeVersion());
         }
     }
 
+    private void saveMatchRequest(ConfirmationMatchRequest matchRequest, StoreHelper.Purpose purpose) {
+        matchRequestStoreHelper.storeForFutureRndmRetrievalDay(matchRequest, purpose);
+    }
 
     // Helper methods
     void setDayForTrdTemplate(long day) {
